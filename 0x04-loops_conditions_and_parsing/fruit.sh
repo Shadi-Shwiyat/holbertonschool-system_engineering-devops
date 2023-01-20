f0f9507a388b4f3a33e30ed7dc5a671f3d57d8e4
Shadi-Shwiyat <raz61103@gmail.com>
2023-01-20 11:32:27 -0800

    Script uses while loop to print "Best School" 10 times

---
+++ 0x04-loops_conditions_and_parsing/fruit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# define an array called fruits
+fruits=("Apple" "Mango" "Pineapple" "Banana" "Orange" "Papaya" "Watermelon")
+len=${#fruits[*]}         # get total elements in an array
+ 
+# print it
+for (( i=0; i<${len}; i++ ));
+do
+	echo "${fruits[$i]}"
+done
